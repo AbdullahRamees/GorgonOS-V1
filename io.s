61f16b04c176d0e8c6c0d54a00753554e01cedd3
Abdullah Ramees <m.abdullah.ramees0916@gmail.com>
2021-08-10 19:21:40 +0530

    Organizing my codes

--- io.s
+++ io.s
@@ -1,4 +1,5 @@
 global outb             ; make the label outb visible outside this file
+global inb
 
 ; outb - send a byte to an I/O port
 ; stack: [esp + 8] the data byte
@@ -10,13 +11,12 @@ outb:
     out dx, al           ; send the data to the I/O port
     ret                  ; return to the calling function
 
-global inb
 
 ; inb - returns a byte from the given I/O port
 ; stack: [esp + 4] The address of the I/O port
 ;        [esp    ] The return address
 inb:
-    mov dx, [esp + 4]    ; move the address of the I/O port to the dx register
-    in  al, dx           ; read a byte from the I/O port and store it in the al
-                         ; register
-    ret                  ; return the read byte
+    mov dx, [esp + 4]       ; move the address of the I/O port to the dx register
+    in  al, dx              ; read a byte from the I/O port and store it in the al register
+    ret                     ; return the read byte
+
