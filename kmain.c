a82109d0a178d840d6c483d8c2f92dd77cf2a750
Abdullah Ramees <73293868+AbdullahRamees@users.noreply.github.com>
2021-09-10 15:39:48 +0000

    Integrate user modes (#7)
    * integrate_user_modes
    
    * updating kmain.c

--- kmain.c
+++ kmain.c
@@ -9,6 +9,8 @@
 
 char message[]= "Gorgon OS";
 
+
+
 int kmain(multiboot_info_t *mbinfo)
 {
 	/* Test appel module 
@@ -27,10 +29,32 @@ int kmain(multiboot_info_t *mbinfo)
 	segments_install_gdt();
 	interrupts_install_idt();
 	
+	init();
+    	
+  	multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+  	multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
+  	unsigned int address_of_module = modules->mod_start;
+  	if((mbinfo->mods_count) == 1){
+  		char message[] = "ONE module loaded successfully!";
+  		serial_write(0x3F8,message,sizeof(message));
+  		
+  		typedef void (*call_module_t)(void);
+        	/* ... */
+        	call_module_t start_program = (call_module_t) address_of_module;
+        	start_program();
+        	/* we'll never get here, unless the module code returns */
+
+  	}else{
+  		char message[] = "Error: More than ONE module loaded";
+  		serial_write(0x3F8,message,sizeof(message));
+  	}
+		
 	init_paging();
 	/* test page fault 	
 	unsigned int *ptr = (unsigned int*)0xA0000000;
    	unsigned int do_page_fault = *ptr;*/
 	
+
 	return 0;
+
 }
